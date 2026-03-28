9fa38c74dc4e646423dadce010e763d27141d16c
shingohorie <shingo.horie.work@gmail.com>
2026-03-29 01:50:13 +0900

    リクエスト過多対策

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,29 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: 'export',
   trailingSlash: true,
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "placehold.jp",
+        protocol: 'https',
+        hostname: 'placehold.jp',
       },
       {
-        protocol: "https",
-        hostname: "samplelib.com",
+        protocol: 'https',
+        hostname: 'samplelib.com',
       },
       {
-        protocol: "https",
-        hostname: "portfolio-s.info",
+        protocol: 'https',
+        hostname: 'portfolio-s.info',
       },
     ],
+  },
+  // ビルド時の並列ワーカー数を制限
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
   },
 };
 
